352687cc52800de6557d23096269ee83b2561013
Alessandro Paris <par_214@hotmail.com>
2022-04-10 23:43:23 -0500

    Added more routes for Products

--- routers/products.py
+++ routers/products.py
@@ -5,22 +5,43 @@ from schemas.product import Product
 from typing import List
 
 
-@router.get("/products/",
-            response_model=List[ProductModel],
-            status_code=status.HTTP_200_OK,
-            tags=["products"])
-async def get_products():
+@router.get("/products", response_model=List[ProductModel], status_code=status.HTTP_200_OK, tags=["products"])
+async def get_all():
     items = db.query(Product).all()
     return items
 
 
-@router.get("/products/{id}",
+@router.get("/products/{product_id}",
             response_model=ProductModel,
             status_code=status.HTTP_200_OK,
             tags=["products"])
-async def get_product(product_id: str):
-    item = db.query(Product).filter(Product.id == product_id).first()
-    if item:
-        return item
+async def get(product_id: str):
+    product = db.query(Product).filter(Product.id == product_id).first()
+    if product:
+        return product
     else:
         raise HTTPException(status_code=404, detail="Resource not found")
+
+
+@router.post("/products",
+             response_model=ProductModel,
+             status_code=status.HTTP_201_CREATED,
+             tags=["products"])
+async def create(product: Product):
+    return product
+
+
+@router.put("/products/{id}",
+            response_model=ProductModel,
+            status_code=status.HTTP_200_OK,
+            tags=["products"])
+async def update(product_id: int):
+    return product_id
+
+
+@router.delete("/products/{id}",
+               response_model=ProductModel,
+               status_code=status.HTTP_200_OK,
+               tags=["products"])
+async def delete(product_id: int):
+    return product_id
