d6868498d94b59fae5b202d4e36b6a26bf88675d
Alessandro Paris <par_214@hotmail.com>
2022-04-11 02:38:26 -0500

    Fixed module import

--- routers/__init__.py
+++ routers/__init__.py
@@ -1,5 +1,7 @@
 from fastapi import APIRouter
+from fastapi.security import OAuth2PasswordBearer
 from db.session import SessionLocal
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 router = APIRouter(prefix="/v1/api")
 db = SessionLocal()
